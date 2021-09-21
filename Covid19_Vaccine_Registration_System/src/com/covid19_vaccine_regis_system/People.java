099cfb5abc9ff7bcc073dd47b9b3025c965577f1
Macelll <72009236+Macelll@users.noreply.github.com>
2021-09-21 14:07:26 +0800

    Add files via upload

---
+++ Covid19_Vaccine_Registration_System/src/com/covid19_vaccine_regis_system/People.java
@@ -0,0 +1,26 @@
+package com.covid19_vaccine_regis_system;
+
+public class People {
+
+    String username;
+    String password;
+
+    public People(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
