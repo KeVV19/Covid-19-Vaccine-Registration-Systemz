35e78e7eb0b5daf9979c14a313fb3dd88d6018f2
Macelll <72009236+Macelll@users.noreply.github.com>
2021-09-29 18:36:55 +0800

    Delete src/com/covid19_vaccine_registration_system directory

--- src/com/covid19_vaccine_registration_system/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.covid19_vaccine_registration_system;
-
-public class Admin extends People {
-
-    public Admin(String username, String password, char gender, int age){
-        super(username, password, gender, age);
-    }
-}
