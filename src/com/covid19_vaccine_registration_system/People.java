99df649aad3a45644d5da3911793a36e457e4678
Macelll <72009236+Macelll@users.noreply.github.com>
2021-09-29 18:31:50 +0800

    Add files via upload

---
+++ src/com/covid19_vaccine_registration_system/People.java
@@ -0,0 +1,35 @@
+package com.covid19_vaccine_registration_system;
+
+import java.io.Serializable;
+
+public class People implements Serializable{
+
+    private String username, password;
+    private char gender;
+    private int age;
+
+    public People(String username, String password, char gender, int age){
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setGender(char gender) { this.gender = gender; }
+    public void setAge (int age) { this.age = age; }
+
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public char getGender() { return gender; }
+    public int getAge() { return age; }
+}
